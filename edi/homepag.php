88678b189cfd76ec5fe5ab9e247c74e9d6f90772
tejas katkade <tejas2002rk@gmail.com>
2022-01-09 12:07:40 +0530

    init

---
+++ edi/homepag.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel='stylesheet' href="homepage.css">
+    <script src="https://kit.fontawesome.com/4c4f7b4f32.js" crossorigin="anonymous"></script>
+    <title>Document</title>
+</head>
+<body>
+    <section class="heading-section">
+        <div class="header container">
+          <div class='logo-section'>
+            <img class="mainlogo" src="LOGO.png">
+           </div> 
+           <div class='section-log'>
+              <ul>
+                  <li><a href="">sign-up</a></li>
+                  <li><a href="">log-in</a></li>
+              </ul>
+           </div> 
+           
+        </div>
+     
+            <div class='centre-content container'>
+              <div class="uppercentre">
+                 <h1 > Musify</h1>
+                 <h2>Listen Music,Live Music</h2>
+                  <a href="mainpage/index.php"> <button class='main-button'>start</button> </a>
+              </div>
+              <div class="lowercentre">
+                 <div><p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Repellendus quia molestiae fugiat culpa voluptatibus laborum.</p>
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum voluptatum a obcaecati cupiditate.</p>
+                </div>
+                  
+                 <a href="#explore"> <button class="explore-btn">explore your intrest</button></a> 
+              </div>
+              
+              
+            </div>
+      
+     
+    </section>
+
+        <div class="explore-section" id='explore'>
+            <div class="box box1">
+                <h1>trending</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, soluta provident dicta illum consequatur voluptates.</p>
+            </div>
+            <div class="box box2"> 
+                <h1>romantic</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, soluta provident dicta illum consequatur voluptates.</p>
+            </div>
+            <div class="box box3">
+                <h1>hip-hop</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, soluta provident dicta illum consequatur voluptates.</p>
+            </div>
+           
+            <div class="box box4">
+                <h1>mix-mashup</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam, soluta provident dicta illum consequatur voluptates.</p>
+            </div>
+        </div>
+    
+  
+    <footer class="footer">
+        <div class="social-media">
+         <i class="fab fa-facebook fa-4x"></i>
+         <i class="fab fa-instagram fa-4x"></i>
+         <i class="fab fa-twitter fa-4x"></i>
+         
+        </div>
+        <p>Musify &copy;2021,ALL RIGHT RESERVED</p>
+     </footer>
+</body>
+</html>
